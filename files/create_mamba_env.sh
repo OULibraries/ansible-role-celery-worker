d23a4c1a9ebde645bc0ee1f8413e60ca47ebc37e
James Mitchell <jamerply@users.noreply.github.com>
2022-04-06 11:31:20 -0500

    add script to initialize mamba environments

---
+++ files/create_mamba_env.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+while getopts ":e:p:v:" opt; do
+    case $opt in
+        e) MAMBA_ENV_NAME="$OPTARG"
+        ;;
+        p) MAMBA_ROOT_PREFIX="$OPTARG"
+        ;;
+        v) PY_VERSION="$OPTARG"
+        ;;
+        \?) "Invalid option -$OPTARG" >&2
+        exit 1
+        ;;
+    esac
+
+    case $OPTARG in
+        -*) echo "Option $opt needs a valida argument"
+        exit 1
+        ;;
+    esac
+done
+
+#PY_VERSION="3.9"
+#MAMBA_ENV_NAME="dev_py39"
+#MAMBA_ROOT_PREFIX=/home/micromamba/jrmitchell
+PATH=/opt/oulib/micromamba/bin:$PATH
+
+export MAMBA_ROOT_PREFIX=$MAMBA_ROOT_PREFIX
+
+# need this for the micromamba commands to work
+eval "$(micromamba shell hook -s posix)"
+
+# create a new python environment with mamba for testing
+micromamba create -y -n "$MAMBA_ENV_NAME" python="$PY_VERSION" -c conda-forge
+
+# activate the test environment
+micromamba activate "$MAMBA_ENV_NAME"
